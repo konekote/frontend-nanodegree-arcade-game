174638656e6fa0902db373a000f90cbdb4c42aef
Laura Degeratu <loveable_koneko@yahoo.com>
2017-04-09 20:14:19 +0200

    add enemy render update and player basic render

--- js/app.js
+++ js/app.js
@@ -1,19 +1,44 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(column = 0, row = 0) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.init(column, row);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
+Enemy.prototype.getInitialXPosition = function(column) {
+  return column * 101 - 101;
+}
+
+Enemy.prototype.getInitialYPosition = function(row) {
+  return row * 83 - 20;
+}
+
+Enemy.prototype.getRandomValueForRange = function(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+Enemy.prototype.XLimit = 505;
+
+Enemy.prototype.init = function(column, row) {
+  this.x = this.getInitialXPosition(column);
+  this.y = this.getInitialYPosition(row);
+  this.viteza = this.getRandomValueForRange(80, 400);
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x = this.x + this.viteza * dt;
+    if(this.x > this.XLimit) {
+      this.init(0, this.getRandomValueForRange(1, 4));
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,13 +50,48 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(column = 0, row = 0) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.init(column, row);
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-horn-girl.png';
+};
+
+Player.prototype.getInitialXPosition = function(column) {
+  return column * 101;
+}
+
+Player.prototype.getInitialYPosition = function(row) {
+  return row * 83 - 20;
+}
+
+Player.prototype.init = function (column, row) {
+  this.x = this.getInitialXPosition(column);
+  this.y = this.getInitialYPosition(row);
+}
+
+Player.prototype.update = function() {
+
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(pressedKey) {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [new Enemy(0,1), new Enemy(0,2), new Enemy(0,3)];
 
-
+var player = new Player(2,5);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
